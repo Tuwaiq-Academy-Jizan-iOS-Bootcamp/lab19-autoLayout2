f4f92458d82076e6e6561e01365d841b57c59ecc
ZHOORHUSSAIN <Zhoor.Hussain.jiz.s@tuwaiq.edu.sa>
2021-11-10 15:46:43 +0300

    zhoor

---
+++ Lab19/Lab19Tests/Lab19Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Lab19Tests.swift
+//  Lab19Tests
+//
+//  Created by زهور حسين on 05/04/1443 AH.
+//
+
+import XCTest
+@testable import Lab19
+
+class Lab19Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
