1174b4deba0091cede21fae4108a7be48a793818
MUSAMASMALI <musa14077@yahoo.com>
2021-11-10 21:55:04 +0300

    musa

---
+++ Autolayout1111/Autolayout1111Tests/Autolayout1111Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Autolayout1111Tests.swift
+//  Autolayout1111Tests
+//
+//  Created by موسى مسملي on 10/11/2021.
+//
+
+import XCTest
+@testable import Autolayout1111
+
+class Autolayout1111Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
