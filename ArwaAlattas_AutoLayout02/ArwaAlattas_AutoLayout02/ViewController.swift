73b3bde7bdef511aa7104722582a4d4b233f6c15
ArwaAlattas <Arwa.Nabeel.jiz.s@tuwaiq.edu.sa>
2021-11-10 18:41:21 +0300

    ArwaAlattas_AutoLayout02

---
+++ ArwaAlattas_AutoLayout02/ArwaAlattas_AutoLayout02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArwaAlattas_AutoLayout02
+//
+//  Created by Arwa Alattas on 05/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
