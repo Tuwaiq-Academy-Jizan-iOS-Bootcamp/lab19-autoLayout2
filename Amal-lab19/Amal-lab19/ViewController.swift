633d1d291c3894e3495933157fc4870503d3f44d
AMALJELI <Aml3mr@hotmail.com>
2021-11-10 16:39:39 +0300

    AMAL-LAB18

---
+++ Amal-lab19/Amal-lab19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amal-lab19
+//
+//  Created by Amal Jeli on 05/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
