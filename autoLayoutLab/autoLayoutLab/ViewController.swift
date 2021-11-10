50ace250e9561370af950bd430cf0cabaa577d66
YOUSEF ALMALKI <mmrdas600@gmail.com>
2021-11-10 19:14:26 +0300

    done

---
+++ autoLayoutLab/autoLayoutLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayoutLab
+//
+//  Created by يوسف جابر المالكي on 05/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
