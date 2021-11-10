57a9bcba5a8208019f13c3ae598d1ec069483fd7
Hanan Somily <hanan.somily.jiz.s@tuwaiq.edu.sa>
2021-11-10 16:36:39 +0300

    HananSomily_ lab19-autoLayout2

---
+++ HananSomily_lab19-autoLayout2/HananSomily_lab19-autoLayout2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HananSomily_lab19-autoLayout2
+//
+//  Created by Hanan Somily on 10/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
