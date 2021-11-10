38c2eac5b5d55bf8212d2280eae0b362940f5c62
GhaziShubayli <ghazi.shubayli.jiz.s@tuwaiq.edu.sa>
2021-11-10 16:31:29 +0300

    done

---
+++ GhaziShubayli_lab19/GhaziShubayli_lab19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhaziShubayli_lab19
+//
+//  Created by ماك بوك on 05/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
