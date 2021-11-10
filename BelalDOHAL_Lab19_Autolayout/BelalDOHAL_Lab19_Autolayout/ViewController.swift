b22bd3927eaa47da63b8664ecb1a83fa2fbac829
Belal <Bealal1995Dohal@gmail.com>
2021-11-10 15:23:10 +0300

    Belal Dohal Lab 19 Autolayout

---
+++ BelalDOHAL_Lab19_Autolayout/BelalDOHAL_Lab19_Autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BelalDOHAL_Lab19_Autolayout
+//
+//  Created by Belal Dohal on 05/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
