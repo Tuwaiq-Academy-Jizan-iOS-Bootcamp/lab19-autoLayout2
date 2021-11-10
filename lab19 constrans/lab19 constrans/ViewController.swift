81d4dc4fa385016bcf4cd46f4f1b5b162354f64c
grand ahmad <grand23@grands-MacBook-Pro.local>
2021-11-10 14:18:49 +0300

    lab19 Atyaf

---
+++ lab19 constrans/lab19 constrans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab19 constrans
+//
+//  Created by grand ahmad on 05/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
