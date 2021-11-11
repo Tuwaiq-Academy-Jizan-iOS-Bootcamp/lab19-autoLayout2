26569cd6ecfec5445f7b899e453bd0b3ab0a1e83
laila hakami <laila.hakami.jiz.s@tuwaiq.edu.sa>
2021-11-11 10:35:30 +0300

    Layla_Lab19_autoLayout12

---
+++ Lab19-autoLayout2/Lab19-autoLayout2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab19-autoLayout2
+//
+//  Created by layla hakami on 06/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
